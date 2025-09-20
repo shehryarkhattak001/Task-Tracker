0afa54c59a4a14c4709ed7f5fd22658da57cf5c4
Shehryar Khan <shehryarkhattak001>
2025-09-20 22:39:02 +0500

    doing the concepts of composition of components

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,30 +1,32 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-const Expenses = () => {
+import Card from "./Card";
+
+const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
-        title={prop.items[0].title}
-        amount={prop.items[0].amount}
-        date={prop.items[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       />
       <ExpenseItem
-        title={prop.items[1].title}
-        amount={prop.items[1].amount}
-        date={prop.items[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       />
       <ExpenseItem
-        title={prop.items[2].title}
-        amount={prop.items[2].amount}
-        date={prop.items[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       />
       <ExpenseItem
-        title={prop.items[3].title}
-        amount={prop.items[3].amount}
-        date={prop.items[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 };
 
