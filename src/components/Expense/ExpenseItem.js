ad27e21c8cc8bafb3e448287454d8fe8ff5ba242
Shehryar Khan <shehryarkhattak001>
2025-09-21 00:35:45 +0500

    add button functiolity and filter chart

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card.js";
+import Card from "../UI/Card";
 
 /* 
   ! useState() 
@@ -16,26 +16,16 @@ import Card from "../UI/Card.js";
 */
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  // console.log("Expense Item Evaluated by React");
-
-  // let title = props.title;
-
-  const clickHandler = () => {
-    // title = "Updated!";
-    setTitle("Updated!!");
-    console.log("Console Updated!");
-  };
-
   return (
-    <Card className="expense-item ">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item ">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
